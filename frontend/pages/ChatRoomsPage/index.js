ec6f297de4d095db6a115c5e4972b03c85844f96
Edward Zhang <edwardz123@gmail.com>
2017-06-24 21:31:10 -0700

    Add sign up page

---
+++ frontend/pages/ChatRoomsPage/index.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Navbar } from '../../components';
+import { ScrollView, Image, View, Text } from 'react-native';
+import { Tabs, Tab, Icon } from 'react-native-elements';
+
+class ChatRoomsPage extends Component {
+
+    renderRooms() {
+        rooms = [{
+                key: 0,
+                img: 'http://i1.kym-cdn.com/entries/icons/facebook/000/019/301/0dc.jpg',
+                description: 'I will destroy ISIS'
+            }, {
+                key: 1,
+                img: 'http://i3.kym-cdn.com/photos/images/original/001/045/025/a90.png',
+                description: 'We have a Linda'
+
+            }];
+
+        const { roomViewStyle } = styles;
+        return rooms.map((room) => {
+            const { key, img,  description} = room;
+            return (
+                <View style={roomViewStyle}
+                    onClick={() => console.log("fuck this shit")}>
+                    <Image
+                        style={{width:64, height:64}}
+                        source={{uri:img}}
+                        />
+                    <View style={{
+                            alignItems:'flex-start',
+                            padding:16,
+                            justifyContent:'center'
+                        }}>
+                        <Text>{description}</Text>
+                    </View>
+                </View>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <View>
+                <Navbar title='Chat Rooms' />
+                <ScrollView>
+                    {this.renderRooms()}
+                </ScrollView>
+            </View>
+        );
+    }
+}
+
+const styles = {
+    roomViewStyle: {
+        backgroundColor:'#eeeeee',
+        margin:4,
+        padding:4,
+        flex:1,
+        flexDirection:'row',
+        justifyContent:'flex-start'
+    }
+}
+
+export default ChatRoomsPage;
--- frontend/pages/HomePage/index.js
+++ frontend/pages/HomePage/index.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Button } from 'react-native-elements';
 import { Actions } from 'react-native-router-flux';
-import { Navbar } from '../../components';
+import { Navbar, Form } from '../../components';
+
 
 class HomePage extends Component {
 	render() {
+		const handlers = [{
+			title: 'Log In',
+			backgroundColor: '#39AEF8',
+			onPress: (value) => {
+			}
+		}, {
+			title: 'Sign In',
+			backgroundColor: '#00C853',
+			onPress: (value) => {
+			}
+		}];
+
 		return (
 			<View>
 				<Navbar title='Home' />
-				<Button 
-					title='Go To Chat' 
+				<Button
+					title='Go To Chat'
 					onPress={() => Actions.chat()}
 				/>
+				<Button
+					title='Go To Chat Rooms Page'
+					onPress={() => Actions.chatRooms()}
+				/>
+			<Form handler={handlers}/>
 			</View>
 		);
 	}
